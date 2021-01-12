2b964c3c90aaaf3e9d9c6fb70eb8ea7069d52d7f
Claudio Capitao <claudio.capitao@gmail.com>
2021-01-12 12:24:57 +0100

    The first files for GiftTree Front-End

---
+++ gifttree/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WelcomeToGiftTree from './WelcomeToGiftTree.js';
+
+function App() {
+  return (
+    <div className="App">
+      <WelcomeToGiftTree />
+    </div>
+  );
+}
+
+export default App;
---
+++ gifttree/src/WelcomeToGiftTree.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export default class WelcomeToGiftTree extends Component {
+    render() {
+        return (
+            <div className='welcome'>
+                <h1>Welcome to Gift Tree</h1>
+            </div>
+        )
+    }
+}
